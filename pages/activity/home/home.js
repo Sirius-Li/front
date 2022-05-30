e72ad9f40877cd6afcce9147eecdb5b371926c4d
adwur <guojiahao1121@outlook.com>
2022-05-30 15:48:52 +0800

    fix home.wxml home.js

--- pages/activity/home/home.js
+++ pages/activity/home/home.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    typeList:['热门话题', '委托推荐', '热门活动'],
+    typeList:['话题推荐', '委托推荐', '热门活动'],
     //获取到的订阅类别
     categories:[],
 
@@ -171,11 +171,11 @@ Page({
         'Authorization': 'Token ' + app.globalData.token
        }
     }
-    if(type == '热门话题'){
+    if(type == '话题推荐'){
       let self = this
       //let templst = []
       wx.request({
-        url: getApp().globalData.baseUrl + '/api/topic_click/',
+        url: getApp().globalData.baseUrl + '/api/topic_recommend/',
         method: 'GET',
         data: {
   
@@ -199,8 +199,8 @@ Page({
     } else if (type == '委托推荐') {
       let self = this
       wx.request({
-        url: getApp().globalData.baseUrl + '/api/commission/search/all/',
-        method: 'GET',
+        url: getApp().globalData.baseUrl + '/api/commission/search/recommend/',
+        method: 'POST',
         data: {
   
         },
@@ -249,7 +249,7 @@ Page({
   getCompoActivityList:function(){
     if (this.data.type == null) {
         this.setData({
-        type: '热门话题'
+        type: '话题推荐'
       })
     }
     let app = getApp()
@@ -265,10 +265,10 @@ Page({
     }
     let self = this
     let type = self.data.type
-    if(type == '热门话题'){
+    if(type == '话题推荐'){
       //let templst = []
       wx.request({
-        url: getApp().globalData.baseUrl + '/api/topic_click/',
+        url: getApp().globalData.baseUrl + '/api/topic_recommend/',
         method: 'GET',
         data: {
   
@@ -291,8 +291,8 @@ Page({
       })
     } else if (type == '委托推荐') {
       wx.request({
-        url: getApp().globalData.baseUrl + '/api/commission/search/all/',
-        method: 'GET',
+        url: getApp().globalData.baseUrl + '/api/commission/search/recommend/',
+        method: 'POST',
         data: {
   
         },
@@ -362,7 +362,7 @@ Page({
           that.setData({
             categories: res.data
           })
-          let tempTypeList = ['热门话题', '委托推荐', '热门活动']
+          let tempTypeList = ['话题推荐', '委托推荐', '热门活动']
           for(let i = 0; i < that.data.categories.length; i++){
             if(that.data.categories[i].id != 1 && that.data.categories[i].id != 5 && that.data.categories[i].id != 9
               && that.data.categories[i].is_subscribe == true){
